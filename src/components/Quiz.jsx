1dd02d007be8b6b61ce0ecd4e51969d5151c36c8
mrybaruk <mykola.rybaruk@pergamonmed.com>
2023-11-07 17:22:29 +0100

    Added shuffling answers and quiz finishing logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,25 +1,40 @@
 import { useState } from "react";
 
-import QUESRIONS from "../questions.js";
+import QUESTIONS from "../questions.js";
+import quizCompleted from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quisIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     console.log(selectedAnswer);
     setUserAnswers((prevUserAnswers) => {
+      console.log(prevUserAnswers);
       return [...prevUserAnswers, selectedAnswer];
     });
   }
 
+  if (quisIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleted} alt="Quis completed" />
+        <h2></h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="questions">
-        <h2>{QUESRIONS[activeQuestionIndex].text}</h2>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESRIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button
                 onClick={() => {
