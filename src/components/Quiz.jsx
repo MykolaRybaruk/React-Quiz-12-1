cce237d3d9472db9cc6e401a6a4cd40cc4b015b8
mrybaruk <mykola.rybaruk@pergamonmed.com>
2023-11-07 16:04:27 +0100

    Connected questions to quis + added the getting shoosed answer logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,37 @@
 import { useState } from "react";
 
+import QUESRIONS from "../questions.js";
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    console.log(selectedAnswer);
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="questions">
+        <h2>{QUESRIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESRIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button
+                onClick={() => {
+                  handleSelectAnswer(answer);
+                }}
+              >
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
