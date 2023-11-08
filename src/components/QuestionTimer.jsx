e50364140da3ebed6e0210f91c5db35adf020aba
Mykola Rybaruk <m.rybaruk@gmail.com>
2023-11-08 13:32:57 +0100

    Added useCallback in Quis.jsx

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,10 +4,12 @@ export default function QuesdtionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainigTime] = useState(timeout);
 
   useEffect(() => {
+    console.log("Setting Timeout");
     setTimeout(onTimeout, timeout);
   }, [onTimeout, timeout]);
 
   useEffect(() => {
+    console.log("Setting Interval");
     setInterval(() => {
       setRemainigTime((prevRemainigTime) => prevRemainigTime - 100);
     }, 100);
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions.js";
 import QuesdtionTimer from "./QuestionTimer.jsx";
@@ -10,13 +10,21 @@ export default function Quiz() {
   const activeQuestionIndex = userAnswers.length;
   const quisIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
     console.log(selectedAnswer);
     setUserAnswers((prevUserAnswers) => {
       console.log(prevUserAnswers);
       return [...prevUserAnswers, selectedAnswer];
     });
-  }
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quisIsComplete) {
     return (
@@ -33,12 +41,7 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="questions">
-        <QuesdtionTimer
-          timeout={10000}
-          onTimeout={() => {
-            handleSelectAnswer(null);
-          }}
-        />
+        <QuesdtionTimer timeout={10000} onTimeout={handleSkipAnswer} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
